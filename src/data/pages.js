ea3df03828770bc661e028f63b254e8ec590ecd3
biodiscus <biodiscus@gmail.com>
2023-07-29 20:43:59 +0200

    refactor: optimize configuration

--- src/data/pages.js
+++ src/data/pages.js
@@ -1,15 +1,15 @@
-const homeData = require('./pages-data/home')
-const aboutData = require('./pages-data/about')
 const pages = [
   {
     path: 'index',
     view: `src/views/home/index.html`,
-    data: homeData
+    // use the path to data file to allow data changes without restarting Webpack
+    data: 'src/data/pages-data/home.js'
   },
   {
     path: 'about/index',
     view: `src/views/about/index.html`,
-    data: aboutData
+    // use the path to data file to allow data changes without restarting Webpack
+    data: 'src/data/pages-data/about.js'
   }
 ]
 
